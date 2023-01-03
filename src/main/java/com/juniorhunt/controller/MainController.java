c90b138573721f954f3d0517e62b5df72a8b3e2f
AlexeyZhigimont <AlexeyZhigimont@gmail.com>
2023-01-03 19:03:16 +0300

    Added security and properties for connect DB

---
+++ src/main/java/com/juniorhunt/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.juniorhunt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String main(){
+        return "home";
+    }
+    @GetMapping("/hello")
+    public String mainPage(){
+        return "hello";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+
+}
